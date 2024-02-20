d93a8854a0bb748646fec158bc96950346597bfd
H <mh.moosavi@fakour.net>
2024-02-20 05:58:14 +0330

    initial data added

---
+++ expense-tracker/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO accounts(id, user_id, name, initial_balance, active) VALUES (1,1,'PayPal', 1000,true);
+INSERT INTO accounts(id, user_id, name, initial_balance, active) VALUES (2,2,'PayPal', 1000,true);
+INSERT INTO categories(id, user_id, name, type) VALUES (1,1,'Salary',1);
+INSERT INTO categories(id, user_id, name, type) VALUES (2,2,'Salary',1);
+INSERT INTO categories(id, user_id, name, type) VALUES (3,1,'Groceries',2);
+INSERT INTO categories(id, user_id, name, type) VALUES (4,2,'Groceries',2);
